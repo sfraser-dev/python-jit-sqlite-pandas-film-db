7c30fd17472d5c4cfaeef6dc2494ff96a57162c5
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-06-28 10:45:20 +0100

    comments

--- replit-copy-files.pl
+++ replit-copy-files.pl
@@ -5,7 +5,11 @@ use strict;
 # Copy "only the files needed to run on replit" to a separate folder.
 # Don't need git files, original-db, project-task pdf, pycache, etc.
 # Easier to upload to replit this way.
-# On replit, select open file, select all files in the folder at once.
+# On replit: select open file, select all files in the folder at once,
+#            upload all the files to the top directory of replit.
+# Note: Instructor Tim said replit works best when all the files are
+#       in the top level directory and there are no sub-folders. Often
+#       get issues if you use sub-folders.
 
 # Copy command will copy and overwrite existing file of same name.
 qx(copy *.py replit-upload-folder\\);
