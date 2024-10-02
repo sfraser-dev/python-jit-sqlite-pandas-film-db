04e12f2933a0bfad71f1bcdc7e24284f69835d1f
Stew Fraser <sif7@outlook.com>
2024-10-02 10:00:11 +0100

    adding python version and reqs

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Python, SQLite and Pandas (command line table views)
 
-Connect to a SQLite DB. Perform SQL queries on this DB. Formats the the table views nicely (normal or retro style) using pandas.
+Connect to a SQLite DB. Perform SQL queries on this DB. Formats the the table views nicely using pandas.
 Writes the formatted table views to the terminal.
 
-Uses pandas and tabulte libraries so need to install them via:
+Uses pandas and tabulte libraries (see requirements.txt):
 
-- `pip install pandas`
-- `pip install tabulate`
+Note to self: See perl file "replit-copy-files.pl" for how to prepare files for upload to replit.
 
-See perl file "replit-copy-files.pl" for how to prepare files for upload to replit.
+To start, run:
+
+- `python -u main.py`
--- replit-copy-files.pl
+++ replit-copy-files.pl
@@ -2,14 +2,14 @@
 use warnings;
 use strict;
 
-# Copy "only the files needed to run on replit" to a separate folder.
-# Don't need git files, original-db, project-task pdf, pycache, etc.
-# Easier to upload to replit this way.
-# On replit: select open file, select all files in the folder at once,
+# Uploading: Copy "only the files needed to run on replit" to a separate folder.
+#            Don't need git files, original-db, project-task pdf, pycache, etc.
+#            Easier to upload to replit this way.
+# On replit: Select open file, select all files in the folder at once,
 #            upload all the files to the top directory of replit.
-# Note: Instructor Tim said replit works best when all the files are
+# Note: Was to that replit works best when all the files are
 #       in the top level directory and there are no sub-folders. Often
-#       get issues if you use sub-folders.
+#       get issues if sub-folders used.
 
 # Copy command will copy and overwrite existing file of same name.
 qx(copy *.py replit-upload-folder\\);
